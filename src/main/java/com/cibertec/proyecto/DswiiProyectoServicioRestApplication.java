36b431cd6f57378d34a5c30eb2ee861fe0bad374
pillaca27 <cristal_27.2011@hotmail.com>
2021-10-28 23:45:16 -0500

    Primer commit

---
+++ src/main/java/com/cibertec/proyecto/DswiiProyectoServicioRestApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiProyectoServicioRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiProyectoServicioRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/proyecto/DswiiProyectoServicioRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiProyectoServicioRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
